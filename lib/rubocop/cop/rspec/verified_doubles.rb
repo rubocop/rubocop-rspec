0115a7e67fad6f4eedb779ca70500cbb477beea2
Andy Waite <andyw8@users.noreply.github.com>
2016-05-02 17:29:05 +0100

    Enhance VerifiedDoubles cop
    Based on the discussions in #35 and #36.
    
    With these changes, I believe it's appropriate to have this cop
    enabled by default.

--- lib/rubocop/cop/rspec/verified_doubles.rb
+++ lib/rubocop/cop/rspec/verified_doubles.rb
@@ -19,13 +19,23 @@ module RuboCop
       #   end
       class VerifiedDoubles < Cop
         MSG = 'Prefer using verifying doubles over normal doubles.'.freeze
+        DOUBLE_TYPES = [:double, :spy].freeze
 
         def on_send(node)
-          _receiver, method_name, *_args = *node
-          return unless method_name == :double
+          _receiver, method_name, *args = *node
+          name, *_stubs = *args
+          return unless DOUBLE_TYPES.include?(method_name)
+          return if name.nil?
+          return if name_is_symbol?(name) && cop_config['IgnoreSymbolicNames']
           add_offense(node,
                       :expression,
                       format(MSG, node.loc.expression.source))
+        end
+
+        private
+
+        def name_is_symbol?(name)
+          name.children.first.is_a? Symbol
         end
       end
     end
