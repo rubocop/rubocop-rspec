7d54edbfd4064b5908d308bdd20ea1216a1e1d1e
Robert Speicher <rspeicher@gmail.com>
2016-05-24 16:14:43 -0400

    Add DescribeSymbol cop
    This cop will add offenses when a Symbol is used as the first argument
    to `describe`.

---
+++ lib/rubocop/cop/rspec/describe_symbol.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Avoid describing symbols.
+      #
+      # @example
+      #   # bad
+      #   describe :my_method do
+      #     ...
+      #   end
+      #
+      #   # good
+      #   describe '#my_method' do
+      #     ...
+      #   end
+      #
+      # See https://github.com/rspec/rspec-core/issues/1610
+      class DescribeSymbol < Cop
+        MSG = 'Avoid describing symbols.'.freeze
+
+        def_node_matcher :describe_symbol?, <<-PATTERN
+          (send {(const nil :RSpec) nil} :describe $sym ...)
+        PATTERN
+
+        def on_send(node)
+          describe_symbol?(node) { |match| add_offense(match, :expression) }
+        end
+      end
+    end
+  end
+end
