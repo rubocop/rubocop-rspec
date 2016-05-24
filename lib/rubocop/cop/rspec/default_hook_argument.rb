c60a86c3b24a9287fe1c591d349a3240c142e228
Robert Speicher <rspeicher@gmail.com>
2016-05-23 21:18:07 -0400

    Add DefaultHookArgument cop
    This cop will add offenses when an RSpec hook method (`before`, `after`,
    `around`) receives the default `:each` or `:example` argument.

---
+++ lib/rubocop/cop/rspec/default_hook_argument.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Omit the default :each argument for RSpec hooks.
+      #
+      # @example
+      #   # bad
+      #   before(:each) do
+      #     ...
+      #   end
+      #
+      #   # good
+      #   before do
+      #     ...
+      #   end
+      class DefaultHookArgument < RuboCop::Cop::Cop
+        MSG = 'Omit the default `:%s` argument for RSpec hooks.'.freeze
+
+        HOOK_METHODS = [:after, :around, :before].freeze
+        DEFAULT_ARGS = [:each, :example].freeze
+
+        def on_send(node)
+          return unless HOOK_METHODS.include?(node.method_name)
+
+          arg_node = node.method_args.first
+          arg, = *arg_node
+
+          return unless DEFAULT_ARGS.include?(arg)
+
+          add_offense(arg_node, :expression, format(MSG, arg))
+        end
+      end
+    end
+  end
+end
