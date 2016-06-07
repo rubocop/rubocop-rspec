f0ea84a4ddbdd458422889236ebe8b9f524b29b6
Cédric Fabianski <cfabianski@me.com>
2016-06-07 18:57:50 +0200

    Complain to the rubocop rules set for this project

--- spec/rubocop/cop/rspec/before_after_all_spec.rb
+++ spec/rubocop/cop/rspec/before_after_all_spec.rb
@@ -3,22 +3,36 @@
 describe RuboCop::Cop::RSpec::BeforeAfterAll do
   subject(:cop) { described_class.new }
 
-  it 'finds a before/after all' do
-    inspect_source(
-      cop,
+  context 'when offenses detected' do
+    let(:code) do
       [
         'describe MyClass do',
         '  before(:all) { do_something}',
         '  after(:all) { do_something_else }',
         'end'
       ]
-    )
-    expect(cop.offenses.size).to eq(2)
-    expect(cop.offenses.map(&:line).sort).to eq([2, 3])
-    expect(cop.messages).to eq([
-      'Avoid the use of before/after(:all) as they are not rolled back and may lead to database state leaking between examples',
-      'Avoid the use of before/after(:all) as they are not rolled back and may lead to database state leaking between examples',
-    ])
+    end
+
+    let(:expected_error) do
+      'Avoid the use of before/after(:all) '\
+        'as they are not rolled back '\
+        'and may lead to database state leaking between examples'
+    end
+
+    it 'reports 2 offenses' do
+      inspect_source(cop, code)
+      expect(cop.offenses.size).to eq(2)
+    end
+
+    it 'reports the lines for these offenses' do
+      inspect_source(cop, code)
+      expect(cop.offenses.map(&:line).sort).to eq([2, 3])
+    end
+
+    it 'describes the offenses' do
+      inspect_source(cop, code)
+      expect(cop.messages).to eq([expected_error, expected_error])
+    end
   end
 
   it 'does not complain for before/after each' do
