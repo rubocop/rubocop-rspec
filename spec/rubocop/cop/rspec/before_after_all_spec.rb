fda5affa6e05a45076513eca1a2fbe817e833da5
Cédric Fabianski <cfabianski@me.com>
2016-06-07 18:02:24 +0200

    Update description to be clearer

--- spec/rubocop/cop/rspec/before_after_all_spec.rb
+++ spec/rubocop/cop/rspec/before_after_all_spec.rb
@@ -16,8 +16,8 @@ describe RuboCop::Cop::RSpec::BeforeAfterAll do
     expect(cop.offenses.size).to eq(2)
     expect(cop.offenses.map(&:line).sort).to eq([2, 3])
     expect(cop.messages).to eq([
-      'Avoid the use of before/after(:all) to avoid specs flakiness when split out',
-      'Avoid the use of before/after(:all) to avoid specs flakiness when split out'
+      'Avoid the use of before/after(:all) as they are not rolled back and leads to flakiness',
+      'Avoid the use of before/after(:all) as they are not rolled back and leads to flakiness',
     ])
   end
 
