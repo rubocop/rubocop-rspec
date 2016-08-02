d5f1f6ab7bf00dd7a333c449aa618e871e187ffa
John Backus <johncbackus@gmail.com>
2016-08-01 22:06:26 -0700

    Refactor DescribeMethod specs

--- spec/rubocop/cop/rspec/describe_method_spec.rb
+++ spec/rubocop/cop/rspec/describe_method_spec.rb
@@ -2,32 +2,31 @@ describe RuboCop::Cop::RSpec::DescribeMethod do
   subject(:cop) { described_class.new }
 
   it 'ignores describes with only a class' do
-    inspect_source(cop, 'describe Some::Class do; end')
-    expect(cop.offenses.empty?).to be(true)
+    expect_violation('describe Some::Class do; end')
   end
 
   it 'enforces non-method names' do
-    inspect_source(
-      cop,
-      "describe Some::Class, 'nope', '.incorrect_usage' do; end"
-    )
-
-    expect(cop.offenses.size).to eq(1)
-    expect(cop.offenses.map(&:line).sort).to eq([1])
-    expect(cop.messages)
-      .to eq(['The second argument to describe should be the method being ' \
-              "tested. '#instance' or '.class'"])
+    expect_violation(<<-RUBY)
+      describe Some::Class, 'nope', '.incorrect_usage' do
+                            ^^^^^^ The second argument to describe should be the method being tested. '#instance' or '.class'
+      end
+    RUBY
   end
 
   it 'skips methods starting with a . or #' do
-    inspect_source(cop, ["describe Some::Class, '.asdf' do; end",
-                         "describe Some::Class, '#fdsa' do; end"])
-    expect(cop.offenses).to be_empty
+    expect_violation(<<-RUBY)
+      describe Some::Class, '.asdf' do
+      end
+
+      describe Some::Class, '#fdsa' do
+      end
+    RUBY
   end
 
   it 'skips specs not having a string second argument' do
-    inspect_source(cop, 'describe Some::Class, :config do; end')
-
-    expect(cop.offenses).to be_empty
+    expect_violation(<<-RUBY)
+      describe Some::Class, :config do
+      end
+    RUBY
   end
 end
