292deae07acca15a275643e4bdc79f4aede2ac7e
John Backus <johncbackus@gmail.com>
2016-08-01 23:04:54 -0700

    Refactor MultipleDescribes spec

--- spec/rubocop/cop/rspec/multiple_describes_spec.rb
+++ spec/rubocop/cop/rspec/multiple_describes_spec.rb
@@ -2,42 +2,27 @@ describe RuboCop::Cop::RSpec::MultipleDescribes do
   subject(:cop) { described_class.new }
 
   it 'finds multiple top level describes with class and method' do
-    inspect_source(
-      cop,
-      [
-        "describe MyClass, '.do_something' do; end",
-        "describe MyClass, '.do_something_else' do; end"
-      ]
-    )
-    expect(cop.offenses.size).to eq(1)
-    expect(cop.offenses.map(&:line).sort).to eq([1])
-    expect(cop.messages).to eq(['Do not use multiple top level describes - ' \
-                                'try to nest them.'])
+    expect_violation(<<-RUBY)
+      describe MyClass, '.do_something' do; end
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Do not use multiple top level describes - try to nest them.
+      describe MyClass, '.do_something_else' do; end
+    RUBY
   end
 
   it 'finds multiple top level describes only with class' do
-    inspect_source(
-      cop,
-      [
-        'describe MyClass do; end',
-        'describe MyOtherClass do; end'
-      ]
-    )
-    expect(cop.offenses.size).to eq(1)
-    expect(cop.offenses.map(&:line).sort).to eq([1])
-    expect(cop.messages).to eq(['Do not use multiple top level describes - ' \
-                                'try to nest them.'])
+    expect_violation(<<-RUBY)
+      describe MyClass do; end
+      ^^^^^^^^^^^^^^^^ Do not use multiple top level describes - try to nest them.
+      describe MyOtherClass do; end
+    RUBY
   end
 
   it 'skips single top level describe' do
-    inspect_source(
-      cop,
-      [
-        "require 'spec_helper'",
-        '',
-        'describe MyClass do; end'
-      ]
-    )
-    expect(cop.offenses).to be_empty
+    expect_violation(<<-RUBY)
+      require 'spec_helper'
+
+      describe MyClass do
+      end
+    RUBY
   end
 end
