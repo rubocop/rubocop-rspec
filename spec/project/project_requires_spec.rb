3cc18690c93a4e8d2e772267eed6f7ec86d039cb
John Backus <johncbackus@gmail.com>
2016-08-05 18:49:55 -0700

    Add project requires spec
    I keep screwing this up and @andyw8 keeps pointing it out so it
    should just be an automated check as part of the build

---
+++ spec/project/project_requires_spec.rb
@@ -0,0 +1,8 @@
+describe 'Project requires' do
+  it 'alphabetizes cop requires' do
+    source   = SpecHelper::ROOT.join('lib', 'rubocop-rspec.rb').read
+    requires = source.split("\n").grep(%r{rubocop/cop})
+
+    expect(requires.join("\n")).to eql(requires.sort.join("\n"))
+  end
+end
