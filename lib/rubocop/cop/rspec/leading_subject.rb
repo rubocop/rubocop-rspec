882bc094a041bcc6779bfc66876415fdcadcca2f
John Backus <johncbackus@gmail.com>
2016-08-08 01:15:48 -0400

    Add LeadingSubject cop
    Fixes #52

---
+++ lib/rubocop/cop/rspec/leading_subject.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Declare test subject before let declarations
+      #
+      # @example
+      #   # bad
+      #   RSpec.describe User do
+      #     let(:params) { blah }
+      #     subject { described_class.new(params) }
+      #
+      #     it 'is valid' do
+      #       expect(subject.valid?).to be(true)
+      #     end
+      #   end
+      #
+      #   # good
+      #   RSpec.describe User do
+      #     subject { described_class.new(params) }
+      #
+      #     let(:params) { blah }
+      #
+      #     it 'is valid' do
+      #       expect(subject.valid?).to be(true)
+      #     end
+      #   end
+      class LeadingSubject < Cop
+        include RuboCop::RSpec::SpecOnly, RuboCop::RSpec::Language
+
+        MSG = 'Declare `subject` above any other `let` declarations'.freeze
+
+        def_node_matcher :subject?, '(block $(send nil :subject ...) args ...)'
+
+        def on_block(node)
+          return unless subject?(node) && !in_spec_block?(node)
+
+          node.parent.each_child_node do |sibling|
+            break if sibling.equal?(node)
+
+            if sibling.method_name.equal?(:let)
+              break add_offense(node, :expression)
+            end
+          end
+        end
+
+        private
+
+        def in_spec_block?(node)
+          node.each_ancestor(:block).any? do |ancestor|
+            Examples::ALL.include?(ancestor.method_name)
+          end
+        end
+      end
+    end
+  end
+end
