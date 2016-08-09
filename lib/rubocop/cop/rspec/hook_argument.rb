1e4a571f60eeb5e793a316767a89bd91172f7768
John Backus <johncbackus@gmail.com>
2016-08-08 23:54:55 -0400

    Add EnforcedStyle to HookArgument

--- lib/rubocop/cop/rspec/hook_argument.rb
+++ lib/rubocop/cop/rspec/hook_argument.rb
@@ -5,9 +5,14 @@ module RuboCop
     module RSpec
       # Omit the default :each argument for RSpec hooks.
       #
-      # @example
+      # @example when configuration is `EnforcedStyle: implicit`
       #   # bad
       #   before(:each) do
+      #     ...
+      #   end
+      #
+      #   # bad
+      #   before(:example) do
       #     ...
       #   end
       #
@@ -15,26 +20,86 @@ module RuboCop
       #   before do
       #     ...
       #   end
+      #
+      # @example when configuration is `EnforcedStyle: each`
+      #   # bad
+      #   before(:example) do
+      #     ...
+      #   end
+      #
+      #   # good
+      #   before do
+      #     ...
+      #   end
+      #
+      #   # good
+      #   before(:each) do
+      #     ...
+      #   end
+      #
+      # @example when configuration is `EnforcedStyle: example`
+      #   # bad
+      #   before(:each) do
+      #     ...
+      #   end
+      #
+      #   # bad
+      #   before do
+      #     ...
+      #   end
+      #
+      #   # good
+      #   before(:example) do
+      #     ...
+      #   end
       class HookArgument < RuboCop::Cop::Cop
-        MSG = 'Omit the default `%p` argument for RSpec hooks.'.freeze
+        include RuboCop::RSpec::Language,
+                ConfigurableEnforcedStyle
 
-        HOOKS = '{:before :after :around}'.freeze
+        IMPLICIT_MSG = 'Omit the default `%p` argument for RSpec hooks.'.freeze
+        EXPLICIT_MSG = 'Use `%p` for RSpec hooks.'.freeze
+
+        HOOKS = "{#{Hooks::ALL.to_node_pattern}}".freeze
+
+        EXPLICIT_STYLES = %i(each example).freeze
 
         def_node_matcher :scoped_hook, <<-PATTERN
-        (block (send nil #{HOOKS} $(sym {:each :example})) ...)
+        (block (send nil #{HOOKS} $(sym ${:each :example})) ...)
         PATTERN
 
         def_node_matcher :unscoped_hook, "(block (send nil #{HOOKS}) ...)"
 
         def on_block(node)
-          hook(node) do |scope|
-            return unless scope
+          hook(node) do |scope, scope_name|
+            return correct_style_detected if scope_name.equal?(style)
+            return check_implicit(node) unless scope
 
-            add_offense(scope, :expression, format(MSG, *scope))
+            style_detected(scope_name)
+            add_offense(scope, :expression, explicit_message(scope_name))
           end
         end
 
         private
+
+        def check_implicit(node)
+          style_detected(:implicit)
+          return if implicit_style?
+
+          method_send, = *node
+          add_offense(method_send, :selector, format(EXPLICIT_MSG, style))
+        end
+
+        def explicit_message(scope)
+          if implicit_style?
+            format(IMPLICIT_MSG, scope)
+          else
+            format(EXPLICIT_MSG, style)
+          end
+        end
+
+        def implicit_style?
+          style.equal?(:implicit)
+        end
 
         def hook(node, &block)
           scoped_hook(node, &block) || unscoped_hook(node, &block)
--- spec/rubocop/cop/rspec/hook_argument_spec.rb
+++ spec/rubocop/cop/rspec/hook_argument_spec.rb
@@ -1,57 +1,161 @@
 # encoding: utf-8
 
-describe RuboCop::Cop::RSpec::HookArgument do
-  subject(:cop) { described_class.new }
+describe RuboCop::Cop::RSpec::HookArgument, :config do
+  subject(:cop) { described_class.new(config) }
 
-  it 'detects :each for hooks' do
-    expect_violation(<<-RUBY)
-      before(:each) { true }
-             ^^^^^ Omit the default `:each` argument for RSpec hooks.
-      after(:each)  { true }
-            ^^^^^ Omit the default `:each` argument for RSpec hooks.
-      around(:each) { true }
-             ^^^^^ Omit the default `:each` argument for RSpec hooks.
-    RUBY
+  let(:cop_config) do
+    { 'EnforcedStyle' => enforced_style }
   end
 
-  it 'detects :example for hooks' do
-    expect_violation(<<-RUBY)
-      before(:example) { true }
-             ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
-      after(:example)  { true }
-            ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
-      around(:example) { true }
-             ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
-    RUBY
+  shared_examples 'ignored hooks' do
+    it 'ignores :context and :suite' do
+      expect_no_violations(<<-RUBY)
+        before(:suite) { true }
+        after(:suite) { true }
+        before(:context) { true }
+        after(:context) { true }
+      RUBY
+    end
+
+    it 'ignores hooks with more than one argument' do
+      expect_no_violations(<<-RUBY)
+        before(:each, :something_custom) { true }
+      RUBY
+    end
+
+    it 'ignores non-rspec hooks' do
+      expect_no_violations(<<-RUBY)
+        setup(:each) { true }
+      RUBY
+    end
   end
 
-  it 'ignores :context and :suite' do
-    expect_no_violations(<<-RUBY)
-      before(:suite) { true }
-      after(:suite) { true }
-      before(:context) { true }
-      after(:context) { true }
-    RUBY
+  shared_examples 'generated config' do |source, detected_style|
+    it 'generates a todo based on the detected style' do
+      inspect_source(cop, source, 'foo_spec.rb')
+
+      expect(cop.config_to_allow_offenses)
+        .to eq('EnforcedStyle' => detected_style)
+    end
   end
 
-  it 'ignores hooks with more than one argument' do
-    expect_no_violations(<<-RUBY)
-      before(:each, :something_custom) { true }
-    RUBY
+  shared_examples 'an example hook' do
+    include_examples 'ignored hooks'
+    include_examples 'generated config', 'before(:each) { foo }', 'each'
+    include_examples 'generated config', 'before(:example) { foo }', 'example'
+    include_examples 'generated config', 'before { foo }', 'implicit'
   end
 
-  it 'ignores non-rspec hooks' do
-    expect_no_violations(<<-RUBY)
-      setup(:each) { true }
-    RUBY
+  context 'when EnforcedStyle is :implicit' do
+    let(:enforced_style) { :implicit }
+
+    it 'detects :each for hooks' do
+      expect_violation(<<-RUBY)
+        before(:each) { true }
+               ^^^^^ Omit the default `:each` argument for RSpec hooks.
+        after(:each)  { true }
+              ^^^^^ Omit the default `:each` argument for RSpec hooks.
+        around(:each) { true }
+               ^^^^^ Omit the default `:each` argument for RSpec hooks.
+      RUBY
+    end
+
+    it 'detects :example for hooks' do
+      expect_violation(<<-RUBY)
+        before(:example) { true }
+               ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
+        after(:example)  { true }
+              ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
+        around(:example) { true }
+               ^^^^^^^^ Omit the default `:example` argument for RSpec hooks.
+      RUBY
+    end
+
+    it 'does not flag hooks without default scopes' do
+      expect_no_violations(<<-RUBY)
+        before { true }
+        after { true }
+        before { true }
+        after { true }
+      RUBY
+    end
+
+    include_examples 'an example hook'
   end
 
-  it 'does not flag hooks without default scopes' do
-    expect_no_violations(<<-RUBY)
-      before { true }
-      after { true }
-      before { true }
-      after { true }
-    RUBY
+  context 'when EnforcedStyle is :each' do
+    let(:enforced_style) { :each }
+
+    it 'detects :each for hooks' do
+      expect_no_violations(<<-RUBY)
+        before(:each) { true }
+        after(:each)  { true }
+        around(:each) { true }
+      RUBY
+    end
+
+    it 'detects :example for hooks' do
+      expect_violation(<<-RUBY)
+        before(:example) { true }
+               ^^^^^^^^ Use `:each` for RSpec hooks.
+        after(:example)  { true }
+              ^^^^^^^^ Use `:each` for RSpec hooks.
+        around(:example) { true }
+               ^^^^^^^^ Use `:each` for RSpec hooks.
+      RUBY
+    end
+
+    it 'does not flag hooks without default scopes' do
+      expect_violation(<<-RUBY)
+        before { true }
+        ^^^^^^ Use `:each` for RSpec hooks.
+        after { true }
+        ^^^^^ Use `:each` for RSpec hooks.
+        before { true }
+        ^^^^^^ Use `:each` for RSpec hooks.
+        after { true }
+        ^^^^^ Use `:each` for RSpec hooks.
+      RUBY
+    end
+
+    include_examples 'an example hook'
+  end
+
+  context 'when EnforcedStyle is :example' do
+    let(:enforced_style) { :example }
+
+    it 'detects :example for hooks' do
+      expect_no_violations(<<-RUBY)
+        before(:example) { true }
+        after(:example)  { true }
+        around(:example) { true }
+      RUBY
+    end
+
+    it 'detects :each for hooks' do
+      expect_violation(<<-RUBY)
+        before(:each) { true }
+               ^^^^^ Use `:example` for RSpec hooks.
+        after(:each)  { true }
+              ^^^^^ Use `:example` for RSpec hooks.
+        around(:each) { true }
+               ^^^^^ Use `:example` for RSpec hooks.
+      RUBY
+    end
+
+    it 'does not flag hooks without default scopes' do
+      expect_violation(<<-RUBY)
+        before { true }
+        ^^^^^^ Use `:example` for RSpec hooks.
+        after { true }
+        ^^^^^ Use `:example` for RSpec hooks.
+        before { true }
+        ^^^^^^ Use `:example` for RSpec hooks.
+        after { true }
+        ^^^^^ Use `:example` for RSpec hooks.
+      RUBY
+    end
+
+    include_examples 'an example hook'
   end
 end
