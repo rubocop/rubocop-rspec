f4f09a6eaee3d2cc3c11e82e0293177bbb33095e
John Backus <johncbackus@gmail.com>
2016-09-01 19:03:01 -0700

    Add ImplicitExpect cop
    Closes #194

---
+++ lib/rubocop/cop/rspec/implicit_expect.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Check that a consistent implict expectation style is used.
+      #
+      # This cop can be configured using the `EnforcedStyle` option
+      # and supports the `--auto-gen-config` flag.
+      #
+      # @example `EnforcedStyle: is_expected`
+      #
+      #   # bad
+      #   it { should be_truthy }
+      #
+      #   # good
+      #   it { is_expected.to be_truthy }
+      #
+      # @example `EnforcedStyle: should`
+      #
+      #   # bad
+      #   it { is_expected.to be_truthy }
+      #
+      #   # good
+      #   it { should be_truthy }
+      #
+      class ImplicitExpect < Cop
+        include RuboCop::RSpec::SpecOnly, ConfigurableEnforcedStyle
+
+        MSG = 'Prefer `%<good>s` over `%<bad>s`.'.freeze
+
+        def_node_matcher :implicit_expect, <<-PATTERN
+          {
+            (send nil ${:should :should_not} ...)
+            (send (send nil $:is_expected) {:to :to_not :not_to} ...)
+          }
+        PATTERN
+
+        alternatives = {
+          'is_expected.to'     => 'should',
+          'is_expected.not_to' => 'should_not',
+          'is_expected.to_not' => 'should_not'
+        }
+
+        ENFORCED_REPLACEMENTS = alternatives.merge(alternatives.invert).freeze
+
+        def on_send(node)
+          return unless (source_range = offending_expect(node))
+
+          expectation_source = source_range.source
+
+          if expectation_source.start_with?(style.to_s)
+            correct_style_detected
+          else
+            opposite_style_detected
+
+            add_offense(node, source_range, offense_message(expectation_source))
+          end
+        end
+
+        private
+
+        def offending_expect(node)
+          case implicit_expect(node)
+          when :is_expected
+            is_expected_range(node.loc)
+          when :should, :should_not
+            node.loc.selector
+          end
+        end
+
+        def is_expected_range(source_map) # rubocop:disable PredicateName
+          Parser::Source::Range.new(
+            source_map.expression.source_buffer,
+            source_map.column,
+            source_map.selector.last_column
+          )
+        end
+
+        def offense_message(offending_source)
+          MSG % {
+            good: ENFORCED_REPLACEMENTS.fetch(offending_source),
+            bad:  offending_source
+          }
+        end
+      end
+    end
+  end
+end
