f3690b63f9eb5c9854f0068f01558020c5dd7b4d
Andy Waite <github.aw@andywaite.com>
2016-12-12 09:04:54 -0500

    Fix specs

--- spec/rubocop/cop/rspec/before_after_all_spec.rb
+++ spec/rubocop/cop/rspec/before_after_all_spec.rb
@@ -20,17 +20,17 @@ describe RuboCop::Cop::RSpec::BeforeAfterAll do
     end
 
     it 'reports 2 offenses' do
-      inspect_source(cop, code)
+      inspect_source(cop, code, 'foo_spec.rb')
       expect(cop.offenses.size).to eq(2)
     end
 
     it 'reports the lines for these offenses' do
-      inspect_source(cop, code)
+      inspect_source(cop, code, 'foo_spec.rb')
       expect(cop.offenses.map(&:line).sort).to eq([2, 3])
     end
 
     it 'describes the offenses' do
-      inspect_source(cop, code)
+      inspect_source(cop, code, 'foo_spec.rb')
       expect(cop.messages).to eq([expected_error, expected_error])
     end
   end
@@ -43,7 +43,8 @@ describe RuboCop::Cop::RSpec::BeforeAfterAll do
         '  before(:each) { do_something }',
         '  after(:each) { do_something_else }',
         'end'
-      ]
+      ],
+      'foo_spec.rb'
     )
     expect(cop.offenses).to be_empty
   end
@@ -56,7 +57,8 @@ describe RuboCop::Cop::RSpec::BeforeAfterAll do
         '  before { do_something }',
         '  after { do_something_else }',
         'end'
-      ]
+      ],
+      'foo_spec.rb'
     )
     expect(cop.offenses).to be_empty
   end
