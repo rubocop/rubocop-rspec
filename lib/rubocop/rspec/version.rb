84726ec9ce64a229a279426f64d85743ece296e3
John Backus <johncbackus@gmail.com>
2017-01-02 22:30:09 -0800

    Bump version to 1.9.1

--- lib/rubocop/rspec/version.rb
+++ lib/rubocop/rspec/version.rb
@@ -4,7 +4,7 @@ module RuboCop
   module RSpec
     # Version information for the RSpec RuboCop plugin.
     module Version
-      STRING = '1.9.0'.freeze
+      STRING = '1.9.1'.freeze
     end
   end
 end
