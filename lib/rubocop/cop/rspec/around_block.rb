5f40929450ce9f592055b9d6b7bbf55b731ff239
Daniel Gollahon <daniel@blockscore.com>
2017-03-03 02:45:50 -0800

    Improve AroundBlock cop
    - Significantly improves test coverage. This cop is now 100% mutation
      tested.
    - Covers additional around scopes such as `:example` or `:context`.
    - Allows string versions of scopes such as `'example'`.
    - Checks that #run or #test is called on the test object (or that the
      object is yielded or passed to another block or method), not just
      that the test argument was referenced.
    - Refactors a bit of naming and test descriptions to be (hopefully) a
      bit clearer.
    - Unifies the two separate hook matchers.

--- lib/rubocop/cop/rspec/around_block.rb
+++ lib/rubocop/cop/rspec/around_block.rb
@@ -24,53 +24,42 @@ module RuboCop
       #     test.run
       #   end
       class AroundBlock < Cop
-        MSG_NO_ARG = 'Test object should be passed to around block'.freeze
+        MSG_NO_ARG     = 'Test object should be passed to around block'.freeze
         MSG_UNUSED_ARG = 'You should call `%<arg>s.call` ' \
-                          'or `%<arg>s.run`'.freeze
+                         'or `%<arg>s.run`'.freeze
 
-        def_node_matcher :scoped_hook, <<-PATTERN
-        (block (send nil :around (sym {:each :example})) $(args ...) ...)
+        def_node_matcher :hook, <<-PATTERN
+          (block {(send nil :around) (send nil :around {sym str})} (args $...) ...)
         PATTERN
 
-        def_node_matcher :unscoped_hook, <<-PATTERN
-        (block (send nil :around) $(args ...) ...)
+        def_node_search :find_arg_usage, <<-PATTERN
+          {(send $... {:call :run}) (send _ _ $...) (yield $...) (block-pass $...)}
         PATTERN
-
-        def_node_search :find_arg_usage, '(lvar $_)'
 
         def on_block(node)
-          hook(node) do |parameters|
-            missing_parameters(parameters) do
-              add_offense(node, :expression, MSG_NO_ARG)
-              return
-            end
-
-            unused_parameters(parameters) do |param, name|
-              add_offense(param, :expression, format(MSG_UNUSED_ARG, arg: name))
+          hook(node) do |(example_proxy)|
+            if example_proxy.nil?
+              add_no_arg_offense(node)
+            else
+              check_for_unused_proxy(node, example_proxy)
             end
           end
         end
 
         private
 
-        def missing_parameters(node)
-          yield if node.children[0].nil?
+        def add_no_arg_offense(node)
+          add_offense(node, :expression, MSG_NO_ARG)
         end
 
-        def unused_parameters(node)
-          first_arg = node.children[0]
-          param, _methods, _args = *first_arg
-          start = node.parent
+        def check_for_unused_proxy(block, proxy)
+          name, = *proxy
 
-          find_arg_usage(start) do |name|
-            return if param == name
+          find_arg_usage(block) do |usage|
+            return if usage.include?(s(:lvar, name))
           end
 
-          yield first_arg, param
-        end
-
-        def hook(node, &block)
-          scoped_hook(node, &block) || unscoped_hook(node, &block)
+          add_offense(proxy, :expression, format(MSG_UNUSED_ARG, arg: name))
         end
       end
     end
