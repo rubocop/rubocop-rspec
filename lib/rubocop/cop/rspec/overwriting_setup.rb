10f635f5461e698737b0ca40e6b2008088924f2b
Maxim Krizhanovsky <maxim@tradeo.com>
2017-03-07 21:51:16 +0200

    Add RSpec/OverwritingSetup cop. Fixes #328

---
+++ lib/rubocop/cop/rspec/overwriting_setup.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec
+      # Checks if there is a let/subject that overwrites an existing one.
+      #
+      # @example
+      #   # bad
+      #     let(:foo) { bar }
+      #     let(:foo) { baz }
+      #
+      #     subject(:foo) { bar }
+      #     let(:foo) { baz }
+      #
+      #     let(:foo) { bar }
+      #     let!(:foo) { baz }
+      #
+      #   # good
+      #     subject(:test) { something }
+      #     let(:foo) { bar }
+      #     let(:baz) { baz }
+      #     let!(:other) { other }
+      class OverwritingSetup < Cop
+        MSG = '`%{name}` is already defined.'.freeze
+
+        def_node_matcher :setup?, <<-PATTERN
+          (block (send nil {:let :let! :subject} (sym $_)) ...)
+        PATTERN
+
+        def on_block(node)
+          return unless example_group?(node)
+
+          _describe, _args, body = *node
+
+          find_duplicates(body) do |duplicate, name|
+            add_offense(duplicate, :expression, format(MSG, name: name))
+          end
+        end
+
+        private
+
+        def find_duplicates(node)
+          setup_expressions = Set.new
+          node.each_child_node do |child|
+            setup?(child) do |name|
+              yield child, name unless setup_expressions.add?(name)
+            end
+          end
+        end
+      end
+    end
+  end
+end
