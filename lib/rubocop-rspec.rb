6374901f62e73b2d67cae907d2a49f7291e5d74c
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-08-10 18:58:08 +0900

    Add new `RSpec/PredicateMatcher` cop

--- lib/rubocop-rspec.rb
+++ lib/rubocop-rspec.rb
@@ -69,6 +69,7 @@ require 'rubocop/cop/rspec/scattered_setup'
 require 'rubocop/cop/rspec/shared_context'
 require 'rubocop/cop/rspec/single_argument_message_chain'
 require 'rubocop/cop/rspec/subject_stub'
+require 'rubocop/cop/rspec/predicate_matcher'
 require 'rubocop/cop/rspec/verified_doubles'
 require 'rubocop/cop/rspec/void_expect'
 
