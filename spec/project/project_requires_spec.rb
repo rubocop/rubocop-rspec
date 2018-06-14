2be2045f0f2e2e84b63beb947a4f093c71b81623
Daniel Gollahon <daniel@blockscore.com>
2018-06-14 02:13:04 -0700

    Fix alphabetic require spec
    - This project structure spec was effectively a no-op because the
      require statements have (presumably) moved since this was added. I
      have fixed the assertion and allowed for varying cop categories as
      well as added a quick sanity check. Having this might be overkill, but
      it least it works now.

--- spec/project/project_requires_spec.rb
+++ spec/project/project_requires_spec.rb
@@ -1,8 +1,18 @@
 RSpec.describe 'Project requires' do
   it 'alphabetizes cop requires' do
-    source   = SpecHelper::ROOT.join('lib', 'rubocop-rspec.rb').read
-    requires = source.split("\n").grep(%r{rubocop/cop/rspec/[^(?:cop)]})
+    source   = SpecHelper::ROOT.join('lib', 'rubocop', 'cop', 'rspec_cops.rb')
+    captures = source.read.scan(%r{^(require_relative 'rspec/(.*?/)?(.*?)')$})
 
-    expect(requires.join("\n")).to eql(requires.sort.join("\n"))
+    require_statements = captures.map(&:first)
+    sorted_require_statements =
+      captures.sort_by do |_require_statement, cop_category, name|
+        [cop_category || 'rspec', name]
+      end.map(&:first)
+
+    aggregate_failures do
+      # Sanity check that we actually discovered require statements.
+      expect(captures).not_to be_empty
+      expect(require_statements).to eql(sorted_require_statements)
+    end
   end
 end
