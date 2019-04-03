6d4d9d9cc14ed28b917cd3825ca3e4f09c917af0
Nils Gemeinhardt <nils.gemeinhardt@xing.com>
2019-04-03 10:41:34 +0200

    Add ContextWorking cop
    Resolves #417
    Fixes #751
    Supersedes #751

---
+++ spec/rubocop/cop/rspec/context_method_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::ContextMethod do
+  subject(:cop) { described_class.new }
+
+  it 'ignores describe blocks' do
+    expect_no_offenses(<<-RUBY)
+      describe '.foo_bar' do
+      end
+
+      describe '#foo_bar' do
+      end
+    RUBY
+  end
+
+  it 'flags context with `.` at the beginning' do
+    expect_offense(<<-RUBY)
+      context '.foo_bar' do
+              ^^^^^^^^^^ Use `describe` for testing methods.
+      end
+    RUBY
+
+    expect_correction(<<-RUBY)
+      describe '.foo_bar' do
+      end
+    RUBY
+  end
+
+  it 'flags context with `#` at the beginning' do
+    expect_offense(<<-RUBY)
+      context '#foo_bar' do
+              ^^^^^^^^^^ Use `describe` for testing methods.
+      end
+    RUBY
+
+    expect_correction(<<-RUBY)
+      describe '#foo_bar' do
+      end
+    RUBY
+  end
+end
