28eab5afecf9a56995618c578b755a0b21a1425f
Andrew Konchin <andry.konchin@gmail.com>
2020-06-27 21:26:16 +0300

    RSpec/LetSetup Optimize #on_block callback

--- lib/rubocop/cop/rspec/let_setup.rb
+++ lib/rubocop/cop/rspec/let_setup.rb
@@ -28,14 +28,20 @@ module RuboCop
       class LetSetup < Cop
         MSG = 'Do not use `let!` to setup objects not referenced in tests.'
 
-        def_node_search :let_bang, <<-PATTERN
+        def_node_matcher :example_or_shared_group_or_including?,
+                         (
+                           ExampleGroups::ALL + SharedGroups::ALL +
+                           Includes::ALL
+                         ).block_pattern
+
+        def_node_matcher :let_bang, <<-PATTERN
           (block $(send nil? :let! (sym $_)) args ...)
         PATTERN
 
         def_node_search :method_called?, '(send nil? %)'
 
         def on_block(node)
-          return unless example_group?(node)
+          return unless example_or_shared_group_or_including?(node)
 
           unused_let_bang(node) do |let|
             add_offense(let)
@@ -45,8 +51,14 @@ module RuboCop
         private
 
         def unused_let_bang(node)
-          let_bang(node) do |method_send, method_name|
+          child_let_bang(node) do |method_send, method_name|
             yield(method_send) unless method_called?(node, method_name)
+          end
+        end
+
+        def child_let_bang(node, &block)
+          RuboCop::RSpec::ExampleGroup.new(node).lets.each do |let|
+            let_bang(let, &block)
           end
         end
       end
