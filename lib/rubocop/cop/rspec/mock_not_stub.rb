8a39c9c5e4a9b32d7ef8f41d910aace02cc8f7ef
Phil Pirozhkov <pirjsuka@gmail.com>
2020-08-08 14:22:00 +0300

    fixup! Add message chains and various configured responses support

--- lib/rubocop/cop/rspec/mock_not_stub.rb
+++ lib/rubocop/cop/rspec/mock_not_stub.rb
@@ -14,41 +14,58 @@ module RuboCop
       #   allow(foo).to receive(:bar).with(42).and_return("hello world")
       #   expect(foo).to receive(:bar).with(42)
       #
-      #   # good (with spies)
-      #   allow(foo).to receive(:bar).with(42).and_return("hello world")
-      #   expect(foo).to have_received(:bar).with(42)
-      #
       class MockNotStub < Cop
         MSG = "Don't stub your mock.".freeze
 
-        def_node_matcher :message_expectation_with_return_argument, <<-PATTERN
-          (send
-            (send nil? :expect ...) :to
-            $(send #receive :and_return _)
-          )
-        PATTERN
-
-        def_node_matcher :message_expectation_with_return_block, <<-PATTERN
-          (send
-            (send nil? :expect ...) :to
-            $(block #receive (args) _)
-          )
-        PATTERN
-
-        def_node_matcher :receive, <<-PATTERN
+        def_node_matcher :message_expectation?, <<-PATTERN
           {
-            (send nil? :receive ...)
+            (send nil? { :receive :receive_message_chain } ...)
             (send (send nil? :receive ...) :with ...)
           }
         PATTERN
 
+        def_node_matcher :configured_response?, <<~PATTERN
+          { :and_return :and_raise :and_throw :and_yield
+            :and_call_original :and_wrap_original }
+        PATTERN
+
+        def_node_matcher :message_expectation_with_configured_response,
+        <<~PATTERN
+          (send
+            (send nil? :expect ...) :to
+            $(send #message_expectation? #configured_response? _)
+          )
+        PATTERN
+
+        def_node_matcher :message_expectation_with_return_block, <<~PATTERN
+          (send
+            (send nil? :expect ...) :to
+            $(block #message_expectation? args _)
+          )
+        PATTERN
+
+        def_node_matcher :messages_expectation_with_configured_hash_response,
+        <<~PATTERN
+          (send
+            (send nil? :expect ...) :to
+            {
+              (send nil? :receive_messages $hash)
+              (send nil? :receive_message_chain ... $hash)
+            }
+          )
+        PATTERN
+
         def on_send(node)
-          message_expectation_with_return_argument(node) do |match|
+          message_expectation_with_configured_response(node) do |match|
             add_offense(offending_argument_range(match.loc))
           end
 
           message_expectation_with_return_block(node) do |match|
             add_offense(offending_block_range(match.loc))
+          end
+
+          messages_expectation_with_configured_hash_response(node) do |match|
+            add_offense(match)
           end
         end
 
--- spec/rubocop/cop/rspec/mock_not_stub_spec.rb
+++ spec/rubocop/cop/rspec/mock_not_stub_spec.rb
@@ -3,35 +3,68 @@
 RSpec.describe RuboCop::Cop::RSpec::MockNotStub do
   subject(:cop) { described_class.new }
 
-  it 'flags expect(...).to receive(...).with(...).and_return' do
-    expect_offense(<<-RUBY)
-      expect(foo).to receive(:bar).with(42).and_return("hello world")
-                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't stub your mock.
-    RUBY
-  end
-
-  it 'flags expect(...).to receive(...).with(...) { } ' do
-    expect_offense(<<-RUBY)
-      expect(foo).to receive(:bar).with(42) { "hello world" }
-                                            ^^^^^^^^^^^^^^^^^ Don't stub your mock.
-    RUBY
-  end
-
-  it 'flags expect(...).to receive(...).and_return' do
+  it 'flags stubbed message expectation' do
     expect_offense(<<-RUBY)
       expect(foo).to receive(:bar).and_return("hello world")
                                   ^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't stub your mock.
     RUBY
   end
 
-  it 'flags expect(...).to receive(...) { } ' do
+  it 'flags stubbed message expectation with a block' do
     expect_offense(<<-RUBY)
       expect(foo).to receive(:bar) { "hello world" }
                                    ^^^^^^^^^^^^^^^^^ Don't stub your mock.
     RUBY
   end
 
-  it 'approves of expect(...).to have_received' do
+  it 'flags stubbed message expectation with argument matching' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive(:bar).with(42).and_return("hello world")
+                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'flags stubbed message expectation with argument matching and a block' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive(:bar).with(42) { "hello world" }
+                                            ^^^^^^^^^^^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'ignores `have_received`' do
     expect_no_offenses('expect(foo).to have_received(:bar)')
+  end
+
+  it 'flags `receive_messages`' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive_messages(foo: 42, bar: 777)
+                                      ^^^^^^^^^^^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'flags `receive_message_chain`' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive_message_chain(:foo, bar: 777)
+                                                 ^^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'flags `receive_message_chain` with `.and_return`' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive_message_chain(:foo, :bar).and_return(777)
+                                                      ^^^^^^^^^^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'flags `receive_message_chain` with a block' do
+    expect_offense(<<-RUBY)
+      expect(foo).to receive_message_chain(:foo, :bar) { 777 }
+                                                       ^^^^^^^ Don't stub your mock.
+    RUBY
+  end
+
+  it 'ignores message allowances' do
+    expect_no_offenses(<<-RUBY)
+    RUBY
   end
 end
