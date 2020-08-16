f7d813c5abe2d4f8e184bb3798ecfebc5e7b45db
Phil Pirozhkov <pirjsuka@gmail.com>
2020-08-16 21:51:16 +0300

    Demonstrate that only the capture from the outer node pattern gets caputred
    In the `expectation` node pattern matcher two captures are also set.
    However, they are not captured/yielded.

--- lib/rubocop/cop/rspec/stubbed_mock.rb
+++ lib/rubocop/cop/rspec/stubbed_mock.rb
@@ -30,37 +30,39 @@ module RuboCop
             :and_call_original :and_wrap_original }
         PATTERN
 
-        def self.expectation_with(matcher)
-          <<~PATTERN
-            (send
-              $(send nil? $#{Expectations::ALL.node_pattern_union} ...)
-              :to #{matcher}
-            )
-          PATTERN
-        end
+        def_node_matcher :expectation, <<~PATTERN
+          (send
+            $(send nil? $#{Expectations::ALL.node_pattern_union} ...)
+            :to %
+          )
+        PATTERN
 
-        def_node_matcher :expectation_with_configured_response,
-                         expectation_with(<<~PATTERN)
-                           (send #message_expectation? #configured_response? _)
-                         PATTERN
+        def_node_matcher :expectation_with_configured_response, <<~PATTERN
+          #expectation(
+            $(send #message_expectation? #configured_response? _)
+          )
+        PATTERN
 
-        def_node_matcher :expectation_with_return_block,
-                         expectation_with(<<~PATTERN)
-                           (block #message_expectation? args _)
-                         PATTERN
+        def_node_matcher :expectation_with_return_block, <<~PATTERN
+          #expectation(
+            $(block #message_expectation? args _)
+          )
+        PATTERN
 
-        def_node_matcher :expectation_with_blockpass_or_hash,
-                         expectation_with(<<~PATTERN)
-                           {
-                             (send nil? { :receive :receive_message_chain } ... block_pass)
-                             (send (send nil? :receive ...) :with ... block_pass)
-                             (send nil? :receive_messages hash)
-                             (send nil? :receive_message_chain ... hash)
-                           }
-                         PATTERN
+        def_node_matcher :expectation_with_blockpass_or_hash, <<~PATTERN
+          #expectation(
+            ${
+              (send nil? { :receive :receive_message_chain } ... block_pass)
+              (send (send nil? :receive ...) :with ... block_pass)
+              (send nil? :receive_messages hash)
+              (send nil? :receive_message_chain ... hash)
+            }
+          )
+        PATTERN
 
         def on_send(node)
           expectation_with_configured_response(node) do |match, method_name|
+            require 'pry'; binding.pry
             add_offense(match, message: msg(method_name))
           end
 
