10fb5a549cc56de784fdc18a00ded715aa77f421
Phil Pirozhkov <pirjsuka@gmail.com>
2020-09-30 11:22:14 +0300

    Use multiple terms union NP syntax

--- lib/rubocop/cop/rspec/subject_stub.rb
+++ lib/rubocop/cop/rspec/subject_stub.rb
@@ -43,10 +43,10 @@ module RuboCop
         #
         #   @yield [Symbol] subject name
         def_node_matcher :subject, <<-PATTERN
-          {
-            (block (send nil? :subject (sym $_)) args ...)
-            (block (send nil? $:subject) args ...)
-          }
+            (block
+              (send nil?
+                {:subject (sym $_) | $:subject}
+              ) args ...)
         PATTERN
 
         # @!method message_expectation?(node, method_name)
