05bccb67040490e2562d77bc960b291bd33c717e
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2022-03-29 10:58:26 +0100

    Exclude all spec blocks from Metrics/BlockLength
    This was something that was originally discussed in https://github.com/rubocop/rubocop-rspec/issues/988

--- lib/rubocop/rspec/config_formatter.rb
+++ lib/rubocop/rspec/config_formatter.rb
@@ -8,6 +8,7 @@ module RuboCop
     class ConfigFormatter
       EXTENSION_ROOT_DEPARTMENT = %r{^(RSpec/)}.freeze
       SUBDEPARTMENTS = %(RSpec/Capybara RSpec/FactoryBot RSpec/Rails)
+      AMENDMENTS = %(Metrics/BlockLength)
       COP_DOC_BASE_URL = 'https://www.rubydoc.info/gems/rubocop-rspec/RuboCop/Cop/RSpec/'
 
       def initialize(config, descriptions)
@@ -18,6 +19,7 @@ module RuboCop
       def dump
         YAML.dump(unified_config)
           .gsub(EXTENSION_ROOT_DEPARTMENT, "\n\\1")
+          .gsub(*AMENDMENTS, "\n\\0")
           .gsub(/^(\s+)- /, '\1  - ')
       end
 
@@ -26,6 +28,7 @@ module RuboCop
       def unified_config
         cops.each_with_object(config.dup) do |cop, unified|
           next if SUBDEPARTMENTS.include?(cop)
+          next if AMENDMENTS.include?(cop)
 
           unified[cop].merge!(descriptions.fetch(cop))
           unified[cop]['Reference'] = COP_DOC_BASE_URL + cop.sub('RSpec/', '')
