59d1b2ebf9804fe3453e56fd19a1f8ef3ca14bd2
ydah <13041216+ydah@users.noreply.github.com>
2022-08-04 10:19:16 +0900

    Add test case for `RSpec/ChangeByZero`
    This PR is adding a test case for writing on a single line
    as shown in the following Issue:
    https://github.com/rubocop/rubocop-rspec/issues/1353

--- spec/rubocop/cop/rspec/change_by_zero_spec.rb
+++ spec/rubocop/cop/rspec/change_by_zero_spec.rb
@@ -29,6 +29,40 @@ RSpec.describe RuboCop::Cop::RSpec::ChangeByZero do
     'with compound expectations' do
     expect_offense(<<-RUBY)
       it do
+        expect { foo }.to change(Foo, :bar).by(0).and change(Foo, :baz).by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                      ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change { Foo.bar }.by(0).and change { Foo.baz }.by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                       ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change(Foo, :bar).by(0) & change(Foo, :baz).by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                    ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change { Foo.bar }.by(0) & change { Foo.baz }.by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                     ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change(Foo, :bar).by(0).or change(Foo, :baz).by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                     ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change { Foo.bar }.by(0).or change { Foo.baz }.by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                      ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change(Foo, :bar).by(0) | change(Foo, :baz).by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                    ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+        expect { foo }.to change { Foo.bar }.by(0) | change { Foo.baz }.by(0)
+                          ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+                                                     ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
+      end
+    RUBY
+
+    expect_no_corrections
+  end
+
+  it 'registers an offense when the argument to `by` is zero ' \
+    'with compound expectations with line break' do
+    expect_offense(<<-RUBY)
+      it do
         expect { foo }
           .to change(Foo, :bar).by(0)
               ^^^^^^^^^^^^^^^^^^^^^^^ Prefer negated matchers with compound expectations over `change.by(0)`.
