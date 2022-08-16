23fd9146166b51756866c7a311a274da72bb7efb
ydah <13041216+ydah@users.noreply.github.com>
2022-08-16 13:03:58 +0900

    Disable `InternalAffairs/NumblockHandler` for `RSpec/DescribedClass`

--- lib/rubocop/cop/rspec/described_class.rb
+++ lib/rubocop/cop/rspec/described_class.rb
@@ -82,7 +82,7 @@ module RuboCop
         def_node_search :contains_described_class?,
                         '(send nil? :described_class)'
 
-        def on_block(node)
+        def on_block(node) # rubocop:disable InternalAffairs/NumblockHandler
           # In case the explicit style is used, we need to remember what's
           # being described.
           @described_class, body = described_constant(node)
