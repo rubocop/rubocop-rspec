54d7b8e27cb70d464868169388523e18df1165c1
r7kamura <r7kamura@gmail.com>
2022-11-23 07:22:26 +0900

    Add `RSpec/MetadataStyle` and `RSpec/EmptyMetadata` cops

---
+++ spec/rubocop/cop/rspec/empty_metadata_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::EmptyMetadata do
+  context 'without metadata' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~RUBY)
+        describe 'Something' do
+        end
+      RUBY
+    end
+  end
+
+  context 'with non-empty metadata hash' do
+    it 'registers no offense' do
+      expect_no_offenses(<<~RUBY)
+        describe 'Something', { a: b } do
+        end
+      RUBY
+    end
+  end
+
+  context 'with empty metadata hash' do
+    it 'registers offense' do
+      expect_offense(<<~RUBY)
+        describe 'Something', {} do
+                              ^^ Avoid empty metadata hash.
+        end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        describe 'Something' do
+        end
+      RUBY
+    end
+  end
+end
