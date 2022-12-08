7c6ebdd33d9e78b92747b22fd61dae0c92774ccf
r7kamura <r7kamura@gmail.com>
2022-12-09 07:15:35 +0900

    Enable `Style/RequireOrder` cop

--- lib/rubocop-rspec.rb
+++ lib/rubocop-rspec.rb
@@ -6,35 +6,39 @@ require 'yaml'
 require 'rubocop'
 
 require_relative 'rubocop/rspec'
-require_relative 'rubocop/rspec/version'
 require_relative 'rubocop/rspec/inject'
-require_relative 'rubocop/rspec/node'
-require_relative 'rubocop/rspec/wording'
 require_relative 'rubocop/rspec/language/node_pattern'
+require_relative 'rubocop/rspec/node'
+require_relative 'rubocop/rspec/version'
+require_relative 'rubocop/rspec/wording'
+
+# Dependent on `RuboCop::RSpec::Language::NodePattern`.
 require_relative 'rubocop/rspec/language'
 
 require_relative 'rubocop/rspec/factory_bot/language'
 
+require_relative 'rubocop/cop/rspec/mixin/capybara_help'
+require_relative 'rubocop/cop/rspec/mixin/css_selector'
+require_relative 'rubocop/cop/rspec/mixin/final_end_location'
+require_relative 'rubocop/cop/rspec/mixin/inside_example_group'
+require_relative 'rubocop/cop/rspec/mixin/metadata'
+require_relative 'rubocop/cop/rspec/mixin/namespace'
+require_relative 'rubocop/cop/rspec/mixin/skip_or_pending'
 require_relative 'rubocop/cop/rspec/mixin/top_level_group'
 require_relative 'rubocop/cop/rspec/mixin/variable'
-require_relative 'rubocop/cop/rspec/mixin/final_end_location'
+
+# Dependent on `RuboCop::Cop::RSpec::FinalEndLocation`.
 require_relative 'rubocop/cop/rspec/mixin/comments_help'
 require_relative 'rubocop/cop/rspec/mixin/empty_line_separation'
-require_relative 'rubocop/cop/rspec/mixin/inside_example_group'
-require_relative 'rubocop/cop/rspec/mixin/namespace'
-require_relative 'rubocop/cop/rspec/mixin/css_selector'
-require_relative 'rubocop/cop/rspec/mixin/skip_or_pending'
-require_relative 'rubocop/cop/rspec/mixin/capybara_help'
-require_relative 'rubocop/cop/rspec/mixin/metadata'
 
-require_relative 'rubocop/rspec/concept'
-require_relative 'rubocop/rspec/example_group'
-require_relative 'rubocop/rspec/example'
-require_relative 'rubocop/rspec/hook'
 require_relative 'rubocop/cop/rspec/base'
 require_relative 'rubocop/rspec/align_let_brace'
-require_relative 'rubocop/rspec/factory_bot'
+require_relative 'rubocop/rspec/concept'
 require_relative 'rubocop/rspec/corrector/move_node'
+require_relative 'rubocop/rspec/example'
+require_relative 'rubocop/rspec/example_group'
+require_relative 'rubocop/rspec/factory_bot'
+require_relative 'rubocop/rspec/hook'
 
 RuboCop::RSpec::Inject.defaults!
 
