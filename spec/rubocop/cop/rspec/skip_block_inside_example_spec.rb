07b514ae68009ee59f2ac448281d675be72eb5a0
Phil Pirozhkov <pirjsuka@gmail.com>
2023-02-05 03:13:05 +0300

    Rename and narrow to `skip` only
    We are not expected to flag `pending`, as it would blow up specs:
    
    https://github.com/rubocop/rubocop-rspec/pull/1566#discussion_r1088923352
    https://github.com/rubocop/rubocop-rspec/pull/1566#discussion_r1089015507
    https://github.com/rspec/rspec-core/blob/79ec0fd5b5ebd13031a10960f293b8a4a43cb734/lib/rspec/core/pending.rb#L66

---
+++ spec/rubocop/cop/rspec/skip_block_inside_example_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::SkipBlockInsideExample, :config do
+  it 'registers an offense when using `skip` with a block' do
+    expect_offense(<<~RUBY)
+      it 'does something' do
+        skip 'not yet implemented' do
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't pass a block to `skip` inside examples.
+        end
+      end
+    RUBY
+  end
+
+  it 'registers an offense when using `skip` with a numblock', :ruby27 do
+    expect_offense(<<~RUBY)
+      it 'does something' do
+        skip 'not yet implemented' do
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't pass a block to `skip` inside examples.
+          _1
+        end
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `skip` without a block' do
+    expect_no_offenses(<<~RUBY)
+      it 'does something' do
+        skip 'not yet implemented'
+      end
+    RUBY
+  end
+
+  it 'does not register an offense outside exxamples' do
+    expect_no_offenses(<<~RUBY)
+      skip 'not yet implemented' do
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using `pending`' do
+    # RSpec would through an ArgumentError
+    expect_no_offenses(<<~RUBY)
+      it 'does something' do
+        pending 'not yet implemented' do
+          # ...
+        end
+        foo 'dfdf' do
+          # ...
+        end
+      end
+    RUBY
+  end
+end
