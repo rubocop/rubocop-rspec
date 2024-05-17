ce42d4c0bbe0e250d0acc223034e8999d1347ca9
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-05-17 18:22:49 +0900

    Add new `RSpec/ExpectInLet` cop

---
+++ spec/rubocop/cop/rspec/expect_in_let_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::RSpec::ExpectInLet do
+  it 'adds an offense for `expect` in let' do
+    expect_offense(<<~RUBY)
+      let(:foo) do
+        expect(something).to eq 'foo'
+        ^^^^^^ Do not use `expect` in let
+        is_expected.to eq 'foo'
+        ^^^^^^^^^^^ Do not use `is_expected` in let
+        expect_any_instance_of(Something).to receive :foo
+        ^^^^^^^^^^^^^^^^^^^^^^ Do not use `expect_any_instance_of` in let
+      end
+    RUBY
+  end
+
+  it 'accepts an empty let' do
+    expect_no_offenses(<<~RUBY)
+      let(:foo) {}
+    RUBY
+  end
+
+  it 'accepts `expect` in `it`' do
+    expect_no_offenses(<<~RUBY)
+      it do
+        expect(something).to eq 'foo'
+        is_expected.to eq 'foo'
+        expect_any_instance_of(Something).to receive :foo
+      end
+    RUBY
+  end
+end
