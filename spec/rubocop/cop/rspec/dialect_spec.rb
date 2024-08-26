fa5da43962b53bffd3565087731e6a98666aac4b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2024-08-26 21:07:32 +0900

    Fix: Fix false negative for `RSpec/Dialect` when specified Capybara-specific methods
    fixed https://github.com/rubocop/rubocop-rspec/issues/1951
    
    `RuboCop::Cop::RSpec::Dialect#rspec_method?` detects if node's second child is an RSpec block using `RuboCop::RSpec::Language::ALL.all`. However, `RuboCop::Cop::RSpec::Dialect#rspec_method?` returned nil because the following Capybara-specific methods were not set in config/default.yml.
    
    - given
    - given!
    - background
    
    I remove the line that checks if it’s a `rspec_method?` and add a `inside_example_group?` check.
    
    cf. https://github.com/rubocop/rubocop-rspec/issues/1951#issuecomment-2296916136
    
    Due to the above, it wouldn't filter out are calls with an explicit receiver where the receiver is not RSpec, eg `foo.describe`. so the following examples were removed.
    
    - allows calling methods named xxx in examples

--- spec/rubocop/cop/rspec/dialect_spec.rb
+++ spec/rubocop/cop/rspec/dialect_spec.rb
@@ -1,59 +1,186 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::RSpec::Dialect do
-  let(:cop_config) do
-    {
-      'PreferredMethods' => {
-        'context' => 'describe'
-      }
-    }
-  end
-
-  it 'allows describe blocks' do
-    expect_no_offenses(<<~RUBY)
-      describe 'display name presence' do
+  context 'with preferred methods' do
+    context 'when `describe` is preferred to `context`' do
+      let(:cop_config) do
+        {
+          'PreferredMethods' => {
+            'context' => 'describe'
+          }
+        }
       end
-    RUBY
-  end
 
-  it 'allows calling methods named context in examples' do
-    expect_no_offenses(<<~RUBY)
-      it 'tests common context invocations' do
-        expect(request.context).to be_empty?
+      it 'allows describe blocks' do
+        expect_no_offenses(<<~RUBY)
+          RSpec.describe 'context' do
+            describe 'display name presence' do
+            end
+          end
+        RUBY
       end
-    RUBY
-  end
 
-  it 'registers an offense for context blocks' do
-    expect_offense(<<~RUBY)
-      context 'display name presence' do
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `describe` over `context`.
-      end
-    RUBY
+      it 'registers an offense for context blocks' do
+        expect_offense(<<~RUBY)
+          RSpec.describe 'context' do
+            context 'display name presence' do
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `describe` over `context`.
+            end
+          end
+        RUBY
 
-    expect_correction(<<~RUBY)
-      describe 'display name presence' do
+        expect_correction(<<~RUBY)
+          RSpec.describe 'context' do
+            describe 'display name presence' do
+            end
+          end
+        RUBY
       end
-    RUBY
-  end
+    end
 
-  it 'registers an offense for RSpec.context blocks' do
-    expect_offense(<<~RUBY)
-      RSpec.context 'context' do
-      ^^^^^^^^^^^^^^^^^^^^^^^ Prefer `describe` over `context`.
-        it 'tests common context invocations' do
-          expect(request.context).to be_empty?
-        end
+    context 'when `describe` is preferred to `feature`' do
+      let(:cop_config) do
+        {
+          'PreferredMethods' => {
+            'feature' => 'describe'
+          }
+        }
       end
-    RUBY
 
-    expect_correction(<<~RUBY)
-      RSpec.describe 'context' do
-        it 'tests common context invocations' do
-          expect(request.context).to be_empty?
-        end
+      it 'allows describe blocks' do
+        expect_no_offenses(<<~RUBY)
+          RSpec.describe 'context' do
+            describe 'display name presence' do
+            end
+          end
+        RUBY
       end
-    RUBY
+
+      it 'registers an offense for feature blocks' do
+        expect_offense(<<~RUBY)
+          RSpec.describe 'context' do
+            feature 'display name presence' do
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Prefer `describe` over `feature`.
+            end
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          RSpec.describe 'context' do
+            describe 'display name presence' do
+            end
+          end
+        RUBY
+      end
+    end
+
+    context 'when `let` is preferred to `given`' do
+      let(:cop_config) do
+        {
+          'PreferredMethods' => {
+            'given' => 'let'
+          }
+        }
+      end
+
+      it 'allows let blocks' do
+        expect_no_offenses(<<~RUBY)
+          RSpec.describe 'context' do
+            let do
+            end
+          end
+        RUBY
+      end
+
+      it 'registers an offense for given blocks' do
+        expect_offense(<<~RUBY)
+          RSpec.describe 'context' do
+            given do
+            ^^^^^ Prefer `let` over `given`.
+            end
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          RSpec.describe 'context' do
+            let do
+            end
+          end
+        RUBY
+      end
+    end
+
+    context 'when `let!` is preferred to `given!`' do
+      let(:cop_config) do
+        {
+          'PreferredMethods' => {
+            'given!' => 'let!'
+          }
+        }
+      end
+
+      it 'allows let! blocks' do
+        expect_no_offenses(<<~RUBY)
+          RSpec.describe 'context' do
+            let! do
+            end
+          end
+        RUBY
+      end
+
+      it 'registers an offense for given! blocks' do
+        expect_offense(<<~RUBY)
+          RSpec.describe 'context' do
+            given! do
+            ^^^^^^ Prefer `let!` over `given!`.
+            end
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          RSpec.describe 'context' do
+            let! do
+            end
+          end
+        RUBY
+      end
+    end
+
+    context 'when `before` is preferred to `background`' do
+      let(:cop_config) do
+        {
+          'PreferredMethods' => {
+            'background' => 'before'
+          }
+        }
+      end
+
+      it 'allows before blocks' do
+        expect_no_offenses(<<~RUBY)
+          RSpec.describe 'context' do
+            before do
+            end
+          end
+        RUBY
+      end
+
+      it 'registers an offense for background blocks' do
+        expect_offense(<<~RUBY)
+          RSpec.describe 'context' do
+            background do
+            ^^^^^^^^^^ Prefer `before` over `background`.
+            end
+          end
+        RUBY
+
+        expect_correction(<<~RUBY)
+          RSpec.describe 'context' do
+            before do
+            end
+          end
+        RUBY
+      end
+    end
   end
 
   context 'without preferred methods' do
@@ -65,9 +192,11 @@ RSpec.describe RuboCop::Cop::RSpec::Dialect do
 
     it 'allows all methods blocks' do
       expect_no_offenses(<<~RUBY)
-        context 'is important' do
-          specify 'for someone to work' do
-            everyone.should have_some_leeway
+        RSpec.describe 'context' do
+          context 'is important' do
+            specify 'for someone to work' do
+              everyone.should have_some_leeway
+            end
           end
         end
       RUBY
