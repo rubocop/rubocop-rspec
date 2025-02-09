e461bad4f4a6fdc35f04a4958806a39ac2b35328
Benjamin Quorning <benjamin@quorning.net>
2025-02-09 13:22:35 +0100

    Improve spec branch coverage for ConfigFormatter
    AMENDMENTS was defined as a String, not an Array. And splatting the
    array into `#gsub` doesn't work if there is more than one element - we
    would need to use `Regexp.union` instead...
    
    but actually I ended up removing AMENDMENTS altogether. All we want to
    do is to add an extra newline before each cop entry. Only new cop
    entries starts at the beginning of line, and contains a slash character
    after the first word. This means we don't need AMENDMENTS at all, and we
    no longer need the capture in EXTENSION_ROOT_DEPARTMENT.

--- lib/rubocop/rspec/config_formatter.rb
+++ lib/rubocop/rspec/config_formatter.rb
@@ -6,8 +6,7 @@ module RuboCop
   module RSpec
     # Builds a YAML config file from two config hashes
     class ConfigFormatter
-      EXTENSION_ROOT_DEPARTMENT = %r{^(RSpec/)}.freeze
-      AMENDMENTS = %(Metrics/BlockLength)
+      EXTENSION_ROOT_DEPARTMENT = %r{^RSpec/}.freeze
       COP_DOC_BASE_URL = 'https://www.rubydoc.info/gems/rubocop-rspec/RuboCop/Cop/'
 
       def initialize(config, descriptions)
@@ -17,8 +16,7 @@ module RuboCop
 
       def dump
         YAML.dump(unified_config)
-          .gsub(EXTENSION_ROOT_DEPARTMENT, "\n\\1")
-          .gsub(*AMENDMENTS, "\n\\0")
+          .gsub(%r{^\w+/}, "\n\\0")
           .gsub(/^(\s+)- /, '\1  - ')
           .gsub('"~"', '~')
       end
@@ -27,8 +25,6 @@ module RuboCop
 
       def unified_config
         cops.each_with_object(config.dup) do |cop, unified|
-          next if AMENDMENTS.include?(cop)
-
           replace_nil(unified[cop])
           unified[cop].merge!(descriptions.fetch(cop))
           unified[cop]['Reference'] = reference(cop)
