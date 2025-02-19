0a0b30588a032c940f3377cf8fa6ae379fb14d3b
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-02-19 19:54:05 +0100

    Remove `Reference` from config
    On https://docs.rubocop.org/rubocop-rspec/3.5/cops_rspec.html, every cop has a reference
    section, linking to the same text on rubydoc.
    
    The reference section is intended for style guide urls and other extra reading for specific cops.
    I don't think linking to rubydoc provides much value, it just dilutes the actual references, so
    maybe we can just remove them?

--- lib/rubocop/rspec/config_formatter.rb
+++ lib/rubocop/rspec/config_formatter.rb
@@ -7,7 +7,6 @@ module RuboCop
     # Builds a YAML config file from two config hashes
     class ConfigFormatter
       EXTENSION_ROOT_DEPARTMENT = %r{^RSpec/}.freeze
-      COP_DOC_BASE_URL = 'https://www.rubydoc.info/gems/rubocop-rspec/RuboCop/Cop/'
 
       def initialize(config, descriptions)
         @config       = config
@@ -27,7 +26,6 @@ module RuboCop
         cops.each_with_object(config.dup) do |cop, unified|
           replace_nil(unified[cop])
           unified[cop].merge!(descriptions.fetch(cop))
-          unified[cop]['Reference'] = reference(cop)
         end
       end
 
@@ -39,10 +37,6 @@ module RuboCop
         config.each do |key, value|
           config[key] = '~' if value.nil?
         end
-      end
-
-      def reference(cop)
-        COP_DOC_BASE_URL + cop
       end
 
       attr_reader :config, :descriptions
