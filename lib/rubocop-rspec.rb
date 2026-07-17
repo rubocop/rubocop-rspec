220376dcb7b22fa415c6a6dd5b9aa447e31b6a2c
Benjamin Quorning <benjamin@quorning.net>
2026-07-17 15:42:47 +0200

    Autoload mixins instead of requiring them
    Based on https://github.com/rubocop/rubocop/commit/b9cda613a6194dbbf146c

--- lib/rubocop-rspec.rb
+++ lib/rubocop-rspec.rb
@@ -12,21 +12,7 @@ require_relative 'rubocop/rspec/plugin'
 require_relative 'rubocop/rspec/version'
 require_relative 'rubocop/rspec/wording'
 
-require_relative 'rubocop/cop/rspec/mixin/file_help'
-require_relative 'rubocop/cop/rspec/mixin/final_end_location'
-require_relative 'rubocop/cop/rspec/mixin/inside_example'
-require_relative 'rubocop/cop/rspec/mixin/inside_example_group'
-require_relative 'rubocop/cop/rspec/mixin/location_help'
-require_relative 'rubocop/cop/rspec/mixin/metadata'
-require_relative 'rubocop/cop/rspec/mixin/namespace'
-require_relative 'rubocop/cop/rspec/mixin/repeated_items'
-require_relative 'rubocop/cop/rspec/mixin/skip_or_pending'
-require_relative 'rubocop/cop/rspec/mixin/top_level_group'
-require_relative 'rubocop/cop/rspec/mixin/variable'
-
-# Dependent on `RuboCop::Cop::RSpec::FinalEndLocation`.
-require_relative 'rubocop/cop/rspec/mixin/comments_help'
-require_relative 'rubocop/cop/rspec/mixin/empty_line_separation'
+require_relative 'rubocop/cop/rspec/mixin'
 
 require_relative 'rubocop/cop/rspec/base'
 require_relative 'rubocop/rspec/align_let_brace'
---
+++ lib/rubocop/cop/rspec/mixin.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module RSpec # rubocop:disable Style/Documentation
+      # Autoloads mixin modules included by cops. Mixins are autoloaded to
+      # reduce the number of # requires because they're used only when the
+      # relevant cop class is loaded.
+
+      autoload :CommentsHelp, "#{__dir__}/mixin/comments_help"
+      autoload :EmptyLineSeparation, "#{__dir__}/mixin/empty_line_separation"
+      autoload :FileHelp, "#{__dir__}/mixin/file_help"
+      autoload :FinalEndLocation, "#{__dir__}/mixin/final_end_location"
+      autoload :InsideExampleGroup, "#{__dir__}/mixin/inside_example_group"
+      autoload :InsideExample, "#{__dir__}/mixin/inside_example"
+      autoload :LocationHelp, "#{__dir__}/mixin/location_help"
+      autoload :Metadata, "#{__dir__}/mixin/metadata"
+      autoload :Namespace, "#{__dir__}/mixin/namespace"
+      autoload :RepeatedItems, "#{__dir__}/mixin/repeated_items"
+      autoload :SkipOrPending, "#{__dir__}/mixin/skip_or_pending"
+      autoload :TopLevelGroup, "#{__dir__}/mixin/top_level_group"
+      autoload :Variable, "#{__dir__}/mixin/variable"
+    end
+  end
+end
